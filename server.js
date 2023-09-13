5f68cd5c3f368c40bb720589dc16b42c5cdd99fc
datorresr <datorresr@gmail.com>
2023-09-13 17:01:30 -0500

    damage

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/reviewers', async function (req, res) {
   }
 })
 
-app.get('/publications', async function (req, res) {
+app.get('/publications1', async function (req, res) {
   try {
     const rows = await pool.query('select r.name, r.publication, r.avatar from reviewers r')
     res.json(rows)
