68de8ed4fae45dd06016b835251490b824771e38
datorresr <datorresr@gmail.com>
2023-10-03 08:32:34 -0500

    VARENV

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql')
 const util = require('util')
 
 const pool = mysql.createPool({
-  host: process.env.DB_ENDPOINT,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME || 'movie_db'
